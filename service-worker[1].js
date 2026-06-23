a9733a2aa041208d28b06ec7faee53b9d4232149
varunpradhang-tech <varun.pradhan.g@gmail.com>
2026-06-23 10:36:07 +0530

    Add files via upload

---
+++ service-worker[1].js
@@ -0,0 +1,24 @@
+const CACHE = "moneybridge-v12";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE).map((key) => caches.delete(key))
+    ))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
